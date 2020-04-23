b13ca13cbfa02c85dd08bb96434dd31daab57010
Himanshu Sharma <himanshu.sharma1103@gmail.com>
2020-04-23 23:49:30 +0530

    add test for newsletter subscription with fake email address

--- src/page/home.page.js
+++ src/page/home.page.js
@@ -3,7 +3,10 @@ import SearchResultsPage from './searchResults.page';
 
 const SELECTORS = {
     SEARCH_INPUT: '#search_query_top',
-    SEARCH_BUTTON: '#searchbox > button'
+    SEARCH_BUTTON: '#searchbox > button',
+    NEWSLETTER_INPUT: '#newsletter-input',
+    SUBMIT_NEWSLETTER_BUTTON: '#newsletter_block_left button',
+    SUCCESS_ALERT_LABEL: '#columns > p'
 };
 class HomePage extends HeaderPage {
     get searchInput() {
@@ -11,6 +14,15 @@ class HomePage extends HeaderPage {
     }
     get searchButton(){
         return $(SELECTORS.SEARCH_BUTTON);
+    }
+    get newsletterInput(){
+        return $(SELECTORS.NEWSLETTER_INPUT);
+    }
+    get submitNewsletterButton(){
+        return $(SELECTORS.SUBMIT_NEWSLETTER_BUTTON);
+    }
+    get successAlertLabel(){
+        return $(SELECTORS.SUCCESS_ALERT_LABEL);
     }
     open(){
         super.open();
@@ -20,6 +32,16 @@ class HomePage extends HeaderPage {
         this.searchInput.setValue(searchText);
         this.searchButton.click();
         return SearchResultsPage;
+    }
+    subscribeToNewsletter(emailAddress){
+        this.newsletterInput.scrollIntoView();
+        this.newsletterInput.setValue(emailAddress);
+        this.submitNewsletterButton.click();
+    }
+    newsLetterSubscriptionAlert(){
+        this.successAlertLabel.waitForDisplayed();
+        this.successAlertLabel.scrollIntoView();
+        return this.successAlertLabel.getText();
     }
 }
 
--- test/specs/functional/yourStore.func.js
+++ test/specs/functional/yourStore.func.js
@@ -1,6 +1,7 @@
 import HomePage from '../../../src/page/home.page';
 import SearchResultsPage from '../../../src/page/searchResults.page';
 import ContactUsPage from '../../../src/page/contactUs.page';
+import * as faker from 'faker';
 
 describe('Customer', () => {
     it('should be able to search for items', () => {
@@ -11,5 +12,9 @@ describe('Customer', () => {
         ContactUsPage.open().contactCustomerService()
             .sendMessage('Customer service', 'abc@abc.com', '1231', 'Order is not delivered');
         expect(ContactUsPage.successMessage()).to.be.equal('Your message has been successfully sent to our team.');
+    });
+    it.only('should be able to subscribe to newsletter', () => {
+        HomePage.open().subscribeToNewsletter(faker.internet.email());
+        expect(HomePage.newsLetterSubscriptionAlert()).to.have.string('Newsletter : You have successfully subscribed to this newsletter.');
     });
 });
